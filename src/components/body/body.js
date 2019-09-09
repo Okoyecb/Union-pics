05c51743bd857f19db70417bec184eff31ef4f3c
Chidi Okoye <okoyecb@gmail.com>
2019-09-09 15:23:55 +0100

    Indentation fixed

--- src/components/body/body.js
+++ src/components/body/body.js
@@ -111,6 +111,98 @@ const cards = () => {
             </div>
           </div>
         </div>
+        <div className="card-wrap">
+          <div className="card">
+            <img src="https://res.cloudinary.com/okoyecb/image/upload/v1557575545/Nissan-QQ_2017-UK-edition_050_figpd4.jpg" className="card-img" alt=""/>
+            <div>
+              <div className="full">
+              </div>
+              <div className="left">
+                <p className="subtitle">00985CE</p>
+                <p className="item">2019</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="card-wrap">
+          <div className="card">
+            <img src="https://res.cloudinary.com/okoyecb/image/upload/v1566914757/photo-1526047932273-341f2a7631f9_m8rv4t.jpg" className="card-img" alt=""/>
+            <div>
+              <div className="full">
+              </div>
+              <div className="left">
+                <p className="subtitle">00985CE</p>
+                <p className="item">2019</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="card-wrap">
+          <div className="card">
+            <img src="https://res.cloudinary.com/okoyecb/image/upload/v1566914944/1000x750-fit-113_wgqfes.jpg" className="card-img" alt=""/>
+            <div>
+              <div className="full">
+              </div>
+              <div className="left">
+                <p className="subtitle">00985CE</p>
+                <p className="item">2019</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="card-wrap">
+          <div className="card">
+            <img src="https://res.cloudinary.com/okoyecb/image/upload/v1557575555/ibiza_b9eedq.jpg" className="card-img" alt=""/>
+            <div>
+              <div className="full">
+              </div>
+              <div className="left">
+                <p className="subtitle">00985CE</p>
+                <p className="item">2019</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="card-wrap">
+          <div className="card">
+            <img src="https://res.cloudinary.com/okoyecb/image/upload/v1557575854/zoeweb-001_p6cdg1.jpg" className="card-img" alt="" />
+            <div>
+              <div className="full">
+              </div>
+              <div className="left">
+                <p className="subtitle">00985CE</p>
+                <p className="item">2019</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="card-wrap">
+          <div className="card">
+            <img src="https://res.cloudinary.com/okoyecb/image/upload/v1566915064/02_01_slide_nature_ioldul.jpg" className="card-img"  alt="" />
+            <div>
+              <div className="full">
+              </div>
+              <div className="left">
+                <p className="subtitle">00985CE</p>
+                <p className="item">2019</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="card-wrap">
+          <div className="card">
+            <img src="https://res.cloudinary.com/okoyecb/image/upload/v1557575541/Nissan_X-Trail_001_0_xacb2p.jpg" className="card-img" alt=""/>
+            <div>
+              <div className="full">
+              </div>
+              <div className="left">
+                <p className="subtitle">00985CE</p>
+                <p className="item">2019</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="card-wrap"></div>
       </div>
         </div>
        </Router>  
