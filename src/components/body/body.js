cf0fa60ef8c35ff607387cd7f0e183f0704abf76
Chidi Okoye <okoyecb@gmail.com>
2019-10-10 12:43:39 +0100

    Light box feature added

--- src/components/body/body.js
+++ src/components/body/body.js
@@ -8,99 +8,99 @@ const cards = () => {
        <Router>
           <div>
           <div className="card-grid">
-          <Card
-            image={'https://res.cloudinary.com/okoyecb/image/upload/v1566914757/photo-1526047932273-341f2a7631f9_m8rv4t.jpg'}
-            subtitle={'00985CE'}
-            item={'2019'}
-          />
-        <Card
-            image={'https://res.cloudinary.com/okoyecb/image/upload/v1566914757/photo-1526047932273-341f2a7631f9_m8rv4t.jpg'}
-            subtitle={'00985CE'}
-            item={'2019'}
-          />
-           <Card
-            image={'https://res.cloudinary.com/okoyecb/image/upload/v1566914944/1000x750-fit-113_wgqfes.jpg'}
-            subtitle={'00985CE'}
-            item={'2019'}
-          />
-          <Card
-            image={'https://res.cloudinary.com/okoyecb/image/upload/v1557575555/ibiza_b9eedq.jpg'}
-            subtitle={'00985CE'}
-            item={'2019'}
-          />
-       
-          <Card
-            image={'https://res.cloudinary.com/okoyecb/image/upload/v1557575854/zoeweb-001_p6cdg1.jpg'}
-            subtitle={'00985CE'}
-            item={'2019'}
-          />
+                  <Card
+                    image={'https://res.cloudinary.com/okoyecb/image/upload/v1566914757/photo-1526047932273-341f2a7631f9_m8rv4t.jpg'}
+                    subtitle={'00985CE'}
+                    item={'2019'}
+                  />
+                <Card
+                    image={'https://res.cloudinary.com/okoyecb/image/upload/v1566914757/photo-1526047932273-341f2a7631f9_m8rv4t.jpg'}
+                    subtitle={'00985CE'}
+                    item={'2019'}
+                  />
+                  <Card
+                    image={'https://res.cloudinary.com/okoyecb/image/upload/v1566914944/1000x750-fit-113_wgqfes.jpg'}
+                    subtitle={'00985CE'}
+                    item={'2019'}
+                  />
+                  <Card
+                    image={'https://res.cloudinary.com/okoyecb/image/upload/v1557575555/ibiza_b9eedq.jpg'}
+                    subtitle={'00985CE'}
+                    item={'2019'}
+                  />
+            
+                <Card
+                  image={'https://res.cloudinary.com/okoyecb/image/upload/v1557575854/zoeweb-001_p6cdg1.jpg'}
+                  subtitle={'00985CE'}
+                  item={'2019'}
+                />
 
-          <Card
-            image={'https://res.cloudinary.com/okoyecb/image/upload/v1566915064/02_01_slide_nature_ioldul.jpg'}
-            subtitle={'00985CE'}
-            item={'2019'}
-          />
+                <Card
+                  image={'https://res.cloudinary.com/okoyecb/image/upload/v1566915064/02_01_slide_nature_ioldul.jpg'}
+                  subtitle={'00985CE'}
+                  item={'2019'}
+                />
 
-          <Card
-            image={'https://res.cloudinary.com/okoyecb/image/upload/v1557575541/Nissan_X-Trail_001_0_xacb2p.jpg'}
-            subtitle={'00985CE'}
-            item={'2019'}
-          />
+                <Card
+                  image={'https://res.cloudinary.com/okoyecb/image/upload/v1557575541/Nissan_X-Trail_001_0_xacb2p.jpg'}
+                  subtitle={'00985CE'}
+                  item={'2019'}
+                />
 
-          <Card
-            image={'https://res.cloudinary.com/okoyecb/image/upload/v1566915209/ecosystem_vx7fag.jpg'}
-            subtitle={'00985CE'}
-            item={'2019'}
-          />
+                <Card
+                  image={'https://res.cloudinary.com/okoyecb/image/upload/v1566915209/ecosystem_vx7fag.jpg'}
+                  subtitle={'00985CE'}
+                  item={'2019'}
+                />
 
-          <Card
-            image={'https://res.cloudinary.com/okoyecb/image/upload/v1557575545/Nissan-QQ_2017-UK-edition_050_figpd4.jpg'}
-            subtitle={'00985CE'}
-            item={'2019'}
-          />
+                <Card
+                  image={'https://res.cloudinary.com/okoyecb/image/upload/v1557575545/Nissan-QQ_2017-UK-edition_050_figpd4.jpg'}
+                  subtitle={'00985CE'}
+                  item={'2019'}
+                />
 
-         <Card
-            image={'https://res.cloudinary.com/okoyecb/image/upload/v1566914757/photo-1526047932273-341f2a7631f9_m8rv4t.jpg'}
-            subtitle={'00985CE'}
-            item={'2019'}
-          />
+              <Card
+                  image={'https://res.cloudinary.com/okoyecb/image/upload/v1566914757/photo-1526047932273-341f2a7631f9_m8rv4t.jpg'}
+                  subtitle={'00985CE'}
+                  item={'2019'}
+                />
 
-          <Card
-            image={'https://res.cloudinary.com/okoyecb/image/upload/v1566914944/1000x750-fit-113_wgqfes.jpg'}
-            subtitle={'00985CE'}
-            item={'2019'}
-          />
+                <Card
+                  image={'https://res.cloudinary.com/okoyecb/image/upload/v1566914944/1000x750-fit-113_wgqfes.jpg'}
+                  subtitle={'00985CE'}
+                  item={'2019'}
+                />
 
-         <Card
-            image={'https://res.cloudinary.com/okoyecb/image/upload/v1557575555/ibiza_b9eedq.jpg'}
-            subtitle={'00985CE'}
-            item={'2019'}
-          />
-          
-          <Card
-            image={'https://res.cloudinary.com/okoyecb/image/upload/v1557575854/zoeweb-001_p6cdg1.jpg'}
-            subtitle={'00985CE'}
-            item={'2019'}
-          />
+              <Card
+                  image={'https://res.cloudinary.com/okoyecb/image/upload/v1557575555/ibiza_b9eedq.jpg'}
+                  subtitle={'00985CE'}
+                  item={'2019'}
+                />
+                
+                <Card
+                  image={'https://res.cloudinary.com/okoyecb/image/upload/v1557575854/zoeweb-001_p6cdg1.jpg'}
+                  subtitle={'00985CE'}
+                  item={'2019'}
+                />
 
-          <Card
-            image={'https://res.cloudinary.com/okoyecb/image/upload/v1566915064/02_01_slide_nature_ioldul.jpg'}
-            subtitle={'00985CE'}
-            item={'2019'}
-          />
+                <Card
+                  image={'https://res.cloudinary.com/okoyecb/image/upload/v1566915064/02_01_slide_nature_ioldul.jpg'}
+                  subtitle={'00985CE'}
+                  item={'2019'}
+                />
 
-          <Card
-            image={'https://res.cloudinary.com/okoyecb/image/upload/v1557575541/Nissan_X-Trail_001_0_xacb2p.jpg'}
-            subtitle={'00985CE'}
-            item={'2019'}
-          />
+                <Card
+                  image={'https://res.cloudinary.com/okoyecb/image/upload/v1557575541/Nissan_X-Trail_001_0_xacb2p.jpg'}
+                  subtitle={'00985CE'}
+                  item={'2019'}
+                />
 
-          <Card
-            image={'https://res.cloudinary.com/okoyecb/image/upload/v1557575854/zoeweb-001_p6cdg1.jpg'}
-            subtitle={'00985CE'}
-            item={'2019'}
-          />
-      </div>
+                <Card
+                  image={'https://res.cloudinary.com/okoyecb/image/upload/v1557575854/zoeweb-001_p6cdg1.jpg'}
+                  subtitle={'00985CE'}
+                  item={'2019'}
+                />
+         </div>
         </div>
        </Router>  
     );
--- src/components/cards/cards.js
+++ src/components/cards/cards.js
@@ -6,7 +6,7 @@ const Card = props => {
   return (
     <div>
     <div className="card-wrap">
-          <div className="card">
+          {/* <div className="card">
             <img src={props.image} className="card-img" alt=""/>
             <div>
               <div className="full">
@@ -16,8 +16,42 @@ const Card = props => {
                 <p className="item">{props.item}</p>
               </div>
             </div>
+          </div> */}
+          <div className="row">
+        <a className="lightbox" href="#photo01">
+          <figure className="photo01"><p /></figure>
+        </a>
+        <div className="left">
+                <p className="subtitle">{props.subtitle}</p>
+                <p className="item">{props.item}</p>
+              </div>
+        <div className="lightbox-target" id="photo01">
+          <div className="container">
+            <img src="https://images.unsplash.com/photo-1518360192974-cb111fe33162?ixlib=rb-0.3.5&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ&s=176a2e53640da93f024168ee870e4ab2" alt=""/>
+            <a className="lightbox-close" href="#">X</a>
           </div>
         </div>
+        <a className="lightbox" href="#photo02">
+          <figure className="photo02"><p></p></figure>
+        </a>
+        <div className="lightbox-target" id="photo02">
+          <div className="container">
+            <img src="https://images.unsplash.com/photo-1524593216312-e3ea9ac26f97?ixlib=rb-0.3.5&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ&s=40ea56ea3a1751a2c61ce161705e4dc0" alt="" />
+            <a className="lightbox-close" href="">X</a>
+          </div>
+        </div>
+        <a className="lightbox" href="#photo03">
+          <figure className="photo03"><p></p></figure>
+        </a>
+        <div className="lightbox-target" id="photo03">
+          <div className="container">
+            <img src="https://images.unsplash.com/photo-1511044231743-fa5f76110c08?ixlib=rb-0.3.5&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ&s=39af28e4d30f32989009e6f18fa8aac7" alt="" />
+            <a className="lightbox-close" href="#">X</a>
+          </div>
+        </div>
+      </div>
+        </div>
+       
     </div>
     
   );
