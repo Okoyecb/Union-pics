febe4f4a1e25b587cbafb7c7985942bd63ec936f
Chidi Okoye <okoyecb@gmail.com>
2019-10-20 23:40:22 +0100

    Lightbox completed

--- src/components/body/body.js
+++ src/components/body/body.js
@@ -1,111 +1,131 @@
-import React from 'react';
-import { BrowserRouter as Router} from "react-router-dom";
+import React, { Component } from 'react';
+import FsLightbox from 'fslightbox-react';
 import Card from '../cards/cards';
 
 
-const cards = () => {
+class Cards extends Component {
+  state = {
+    toggler: false,
+    slide: 1
+  }
+
+  openLightboxOnSlide = (slideNumber) => {
+    this.setState({ 
+      toggler: !this.state.toggler,
+      slide: slideNumber
+    });
+  }
+
+  render(){
     return(
-       <Router>
-          <div>
-          <div className="card-grid">
+      <>
+      <div className="card-grid">
+        <Card
+            image={'https://res.cloudinary.com/okoyecb/image/upload/v1566914757/photo-1526047932273-341f2a7631f9_m8rv4t.jpg'}
+            subtitle={'00985CE'}
+            item={'2019'}
+            showLightbox={ this.openLightboxOnSlide.bind(this, 1)}
+          />
                   <Card
-                    image={'https://res.cloudinary.com/okoyecb/image/upload/v1566914757/photo-1526047932273-341f2a7631f9_m8rv4t.jpg'}
+                    image={'https://res.cloudinary.com/okoyecb/image/upload/v1557575555/ibiza_b9eedq.jpg'}
                     subtitle={'00985CE'}
                     item={'2019'}
-                  />
-                <Card
-                    image={'https://res.cloudinary.com/okoyecb/image/upload/v1566914757/photo-1526047932273-341f2a7631f9_m8rv4t.jpg'}
-                    subtitle={'00985CE'}
-                    item={'2019'}
+                    showLightbox={ this.openLightboxOnSlide.bind(this, 2) }
                   />
                   <Card
                     image={'https://res.cloudinary.com/okoyecb/image/upload/v1566914944/1000x750-fit-113_wgqfes.jpg'}
                     subtitle={'00985CE'}
                     item={'2019'}
+                    showLightbox={ this.openLightboxOnSlide.bind(this, 3) }
                   />
-                  <Card
+
+                <Card
                     image={'https://res.cloudinary.com/okoyecb/image/upload/v1557575555/ibiza_b9eedq.jpg'}
                     subtitle={'00985CE'}
                     item={'2019'}
+                    showLightbox={ this.openLightboxOnSlide.bind(this, 4) }
                   />
-            
+          
                 <Card
                   image={'https://res.cloudinary.com/okoyecb/image/upload/v1557575854/zoeweb-001_p6cdg1.jpg'}
                   subtitle={'00985CE'}
                   item={'2019'}
-                />
+                  showLightbox={ this.openLightboxOnSlide.bind(this, 5) }
+                 />
 
                 <Card
                   image={'https://res.cloudinary.com/okoyecb/image/upload/v1566915064/02_01_slide_nature_ioldul.jpg'}
                   subtitle={'00985CE'}
                   item={'2019'}
+                  showLightbox={ this.openLightboxOnSlide.bind(this, 6) }
                 />
 
-                <Card
-                  image={'https://res.cloudinary.com/okoyecb/image/upload/v1557575541/Nissan_X-Trail_001_0_xacb2p.jpg'}
+                 <Card
+                   image={'https://res.cloudinary.com/okoyecb/image/upload/v1557575541/Nissan_X-Trail_001_0_xacb2p.jpg'}
+                   subtitle={'00985CE'}
+                  item={'2019'}
+                  showLightbox={ this.openLightboxOnSlide.bind(this, 7) }
+                 />
+
+                 <Card
+                   image={'https://res.cloudinary.com/okoyecb/image/upload/v1566914944/1000x750-fit-113_wgqfes.jpg'}
+                   subtitle={'00985CE'}
+                   item={'2019'}
+                   showLightbox={ this.openLightboxOnSlide.bind(this, 8) }
+                 />
+
+               <Card
+                   image={'https://res.cloudinary.com/okoyecb/image/upload/v1557575555/ibiza_b9eedq.jpg'}
+                   subtitle={'00985CE'}
+                   item={'2019'}
+                   showLightbox={ this.openLightboxOnSlide.bind(this, 9) }
+                 />
+                
+                 <Card
+                   image={'https://res.cloudinary.com/okoyecb/image/upload/v1557575545/Nissan-QQ_2017-UK-edition_050_figpd4.jpg'}
+                   subtitle={'00985CE'}
+                   item={'2019'}
+                   showLightbox={ this.openLightboxOnSlide.bind(this, 10) }
+                />
+
+               <Card                   
+                   image={'https://res.cloudinary.com/okoyecb/image/upload/v1566915064/02_01_slide_nature_ioldul.jpg'}
                   subtitle={'00985CE'}
                   item={'2019'}
-                />
+                  showLightbox={ this.openLightboxOnSlide.bind(this, 11) }
+               />
 
-                <Card
+                <Card                  
                   image={'https://res.cloudinary.com/okoyecb/image/upload/v1566915209/ecosystem_vx7fag.jpg'}
                   subtitle={'00985CE'}
                   item={'2019'}
+                  showLightbox={ this.openLightboxOnSlide.bind(this, 12) }
                 />
+          <FsLightbox
+            toggler={this.state.toggler}
+            sources={ [
+              'https://res.cloudinary.com/okoyecb/image/upload/v1566914757/photo-1526047932273-341f2a7631f9_m8rv4t.jpg',
+              'https://res.cloudinary.com/okoyecb/image/upload/v1557575555/ibiza_b9eedq.jpg',
+              'https://res.cloudinary.com/okoyecb/image/upload/v1566914944/1000x750-fit-113_wgqfes.jpg',
+              'https://res.cloudinary.com/okoyecb/image/upload/v1557575555/ibiza_b9eedq.jpg',
+              'https://res.cloudinary.com/okoyecb/image/upload/v1557575854/zoeweb-001_p6cdg1.jpg',
+              'https://res.cloudinary.com/okoyecb/image/upload/v1566915064/02_01_slide_nature_ioldul.jpg',
+              'https://res.cloudinary.com/okoyecb/image/upload/v1557575541/Nissan_X-Trail_001_0_xacb2p.jpg',
+              'https://res.cloudinary.com/okoyecb/image/upload/v1566914944/1000x750-fit-113_wgqfes.jpg',
+              'https://res.cloudinary.com/okoyecb/image/upload/v1557575555/ibiza_b9eedq.jpg',
+              'https://res.cloudinary.com/okoyecb/image/upload/v1557575545/Nissan-QQ_2017-UK-edition_050_figpd4.jpg',
+              'https://res.cloudinary.com/okoyecb/image/upload/v1566915064/02_01_slide_nature_ioldul.jpg',
+              'https://res.cloudinary.com/okoyecb/image/upload/v1566915209/ecosystem_vx7fag.jpg'
+              ] }
+              slide={this.state.slide}
+          />
+      </div>
+      
 
-                <Card
-                  image={'https://res.cloudinary.com/okoyecb/image/upload/v1557575545/Nissan-QQ_2017-UK-edition_050_figpd4.jpg'}
-                  subtitle={'00985CE'}
-                  item={'2019'}
-                />
+      </>
+    )
 
-              <Card
-                  image={'https://res.cloudinary.com/okoyecb/image/upload/v1566914757/photo-1526047932273-341f2a7631f9_m8rv4t.jpg'}
-                  subtitle={'00985CE'}
-                  item={'2019'}
-                />
-
-                <Card
-                  image={'https://res.cloudinary.com/okoyecb/image/upload/v1566914944/1000x750-fit-113_wgqfes.jpg'}
-                  subtitle={'00985CE'}
-                  item={'2019'}
-                />
-
-              <Card
-                  image={'https://res.cloudinary.com/okoyecb/image/upload/v1557575555/ibiza_b9eedq.jpg'}
-                  subtitle={'00985CE'}
-                  item={'2019'}
-                />
-                
-                <Card
-                  image={'https://res.cloudinary.com/okoyecb/image/upload/v1557575854/zoeweb-001_p6cdg1.jpg'}
-                  subtitle={'00985CE'}
-                  item={'2019'}
-                />
-
-                <Card
-                  image={'https://res.cloudinary.com/okoyecb/image/upload/v1566915064/02_01_slide_nature_ioldul.jpg'}
-                  subtitle={'00985CE'}
-                  item={'2019'}
-                />
-
-                <Card
-                  image={'https://res.cloudinary.com/okoyecb/image/upload/v1557575541/Nissan_X-Trail_001_0_xacb2p.jpg'}
-                  subtitle={'00985CE'}
-                  item={'2019'}
-                />
-
-                <Card
-                  image={'https://res.cloudinary.com/okoyecb/image/upload/v1557575854/zoeweb-001_p6cdg1.jpg'}
-                  subtitle={'00985CE'}
-                  item={'2019'}
-                />
-         </div>
-        </div>
-       </Router>  
-    );
+  }
 }
-export default cards;
 
-
-
+export default Cards;
---
+++ src/components/photos.js
@@ -0,0 +1,53 @@
+// import React from 'react';
+// import { BrowserRouter as Router} from "react-router-dom";
+// import Card from '../cards/cards';
+
+
+// export const photos = [
+//     {
+//         src: 'https://res.cloudinary.com/okoyecb/image/upload/v1566914757/photo-1526047932273-341f2a7631f9_m8rv4t.jpg',
+//                            subtitle:'00985CE',
+//                            item: '2019'
+//     },
+//     {
+//       src: "https://source.unsplash.com/Dm-qxdynoEc/800x799",
+//       width: 1,
+//       height: 1
+//     },
+//     {
+//       src: "https://source.unsplash.com/qDkso9nvCg0/600x799",
+//       width: 3,
+//       height: 4
+//     },
+//     {
+//       src: "https://source.unsplash.com/iecJiKe_RNg/600x799",
+//       width: 3,
+//       height: 4
+//     },
+//     {
+//       src: "https://source.unsplash.com/epcsn8Ed8kY/600x799",
+//       width: 3,
+//       height: 4
+//     },
+//     {
+//       src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
+//       width: 4,
+//       height: 3
+//     },
+//     {
+//       src: "https://source.unsplash.com/zh7GEuORbUw/600x799",
+//       width: 3,
+//       height: 4
+//     },
+//     {
+//       src: "https://source.unsplash.com/PpOHJezOalU/800x599",
+//       width: 4,
+//       height: 3
+//     },
+//     {
+//       src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
+//       width: 4,
+//       height: 3
+//     }
+//   ];
+  
